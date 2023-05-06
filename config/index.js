acbf3a087a2335d46286e41b47561ea9255fa845
kazenoyume <wind.dream.kazenoyume@gmail.com>
2023-05-06 20:01:01 +0800

    調整timeout預設秒數

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const config = Object.freeze({
   APP_PORT: env.APP_PORT || null,
   APP_LANG: env.APP_LANG || 'zh_TW',
   APP_WEBHOOK_PATH: env.APP_WEBHOOK_PATH || '/webhook',
-  APP_API_TIMEOUT: env.APP_API_TIMEOUT || 9000,
+  APP_API_TIMEOUT: env.APP_API_TIMEOUT || 180000,
   APP_MAX_GROUPS: Number(env.APP_MAX_GROUPS) || 5,
   APP_MAX_USERS: Number(env.APP_MAX_USERS) || 100,
   APP_MAX_PROMPT_MESSAGES: Number(env.APP_MAX_PROMPT_MESSAGES) || 12,
